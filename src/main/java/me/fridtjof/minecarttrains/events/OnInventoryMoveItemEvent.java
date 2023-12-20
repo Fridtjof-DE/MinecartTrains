373c772a80b07991db3801c2b9471ab6b4529538
Fridtjof_DE <38737231+Fridtjof-DE@users.noreply.github.com>
2023-12-20 16:14:29 +0100

    fixed itemstack blocking loading issue

--- src/main/java/me/fridtjof/minecarttrains/events/OnInventoryMoveItemEvent.java
+++ src/main/java/me/fridtjof/minecarttrains/events/OnInventoryMoveItemEvent.java
@@ -6,7 +6,11 @@ import org.bukkit.block.Hopper;
 import org.bukkit.entity.minecart.StorageMinecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
 
 public class OnInventoryMoveItemEvent implements Listener
 {
@@ -35,12 +39,16 @@ public class OnInventoryMoveItemEvent implements Listener
         }
     }
 
-    //allow hoppers to fill just coal in carts
-    //TODO if a slot is occupied by a non coal item, the slots after that wont be unloaded
+    //prevent hoppers from filling non coal items in carts
     @EventHandler
     public void onMoveItemIntoFuelCartEvent(InventoryMoveItemEvent event)
     {
         if(!(event.getDestination().getHolder() instanceof StorageMinecart))
+        {
+            return;
+        }
+
+        if(!(event.getSource().getHolder() instanceof Hopper))
         {
             return;
         }
@@ -53,6 +61,16 @@ public class OnInventoryMoveItemEvent implements Listener
         if(isFuelCart((StorageMinecart) event.getDestination().getHolder()))
         {
             event.setCancelled(true);
+
+            //rearrange to possibly let other itemstacks get through
+            if(!rearrangeHopper(event.getSource(), Material.COAL))
+            {
+                if(!rearrangeHopper(event.getSource(), Material.CHARCOAL))
+                {
+                    //rearranging was not successful
+                    return;
+                }
+            }
         }
     }
 
@@ -69,5 +87,37 @@ public class OnInventoryMoveItemEvent implements Listener
         }
 
         return false;
+    }
+
+    public boolean rearrangeHopper(Inventory inventory, Material wantedMaterial)
+    {
+
+        //check for item
+        if(!inventory.contains(wantedMaterial))
+        {
+            return false;
+        }
+
+        //check where item is at
+        int slot = 0;
+        for(ItemStack itemStack : inventory.getContents())
+        {
+
+            if((itemStack != null) && (itemStack.getType() == wantedMaterial))
+            {
+                break;
+            }
+            slot++;
+        }
+
+        //get itemstacks to rearrange
+        ItemStack firstItemStack = inventory.getItem(0);
+        ItemStack wantedItemStack = inventory.getItem(slot);
+
+        //rearrange itemstacks
+        inventory.setItem(0, wantedItemStack);
+        inventory.setItem(slot, firstItemStack);
+
+        return true;
     }
 }
